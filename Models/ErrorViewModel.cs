bc30061d139d083af9de15ce1dc5688961d3307f
nsmontar <nsmontar@gmail.com>
2021-07-23 10:41:25 +0200

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotNetCoreAppLifetimeEvents.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
