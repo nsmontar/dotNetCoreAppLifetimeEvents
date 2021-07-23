ccadb09a861a87c4ea29836b006333691a11fa47
nsmontar <nsmontar@gmail.com>
2021-07-23 14:40:07 +0200

    Completed app shutdown functionality using HostApplicationLifetime

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,21 +6,39 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using DotNetCoreAppLifetimeEvents.Models;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace DotNetCoreAppLifetimeEvents.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHostApplicationLifetime _lifetime;
+        private readonly IMemoryCache _cache;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IHostApplicationLifetime lifetime, IMemoryCache cache)
         {
             _logger = logger;
+            _lifetime = lifetime;
+            _cache = cache;
         }
 
         public IActionResult Index()
         {
+            if (_cache.Get("timestamp") == null)
+            {
+                _cache.Set<string>("timestamp", DateTime.Now.ToString());
+            }
+            ViewBag.TimeStamp = _cache.Get<string>("timestamp");
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Stop()
+        {
+            _lifetime.StopApplication();
+            return new ObjectResult("Application stopped successfully");
         }
 
         public IActionResult Privacy()
--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -13,6 +14,7 @@ namespace DotNetCoreAppLifetimeEvents
 {
     public class Startup
     {
+        string path;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -24,11 +26,14 @@ namespace DotNetCoreAppLifetimeEvents
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddMvc();
+            services.AddMemoryCache();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHostApplicationLifetime lifetime)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -52,6 +57,30 @@ namespace DotNetCoreAppLifetimeEvents
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            path = $"{env.WebRootPath}/AppLog.txt";
+
+            lifetime.ApplicationStarted.Register(OnAppStarted);
+            lifetime.ApplicationStopping.Register(OnAppStopping);
+            lifetime.ApplicationStopped.Register(OnAppStopped);
+        }
+
+        public void OnAppStarted() 
+        {
+            string contents = $"App started at {DateTime.Now}\n";
+            File.AppendAllText(path, contents);
+        }
+
+        public void OnAppStopping()
+        {
+            string contents = $"App stopping at {DateTime.Now}\n";
+            File.AppendAllText(path, contents);
+        }
+
+        public void OnAppStopped()
+        {
+            string contents = $"App stopped at {DateTime.Now}\n";
+            File.AppendAllText(path, contents);
         }
     }
 }
